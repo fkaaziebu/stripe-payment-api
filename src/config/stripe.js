3a6bed765086ec3f5a2ed63498c81567c6c8ffb9
Frederick Aziebu <66077189+fkaaziebu@users.noreply.github.com>
2023-10-11 19:23:28 +0000

    Added key in code

--- src/config/stripe.js
+++ src/config/stripe.js
@@ -1,6 +1,6 @@
 const Stripe = require("stripe");
 
-const stripe = Stripe(process.env.STRIPE_API_KEY, {
+const stripe = Stripe('sk_test_51NrPGIC1JzpCkXscdeQjqJj2zSf3lGVjWTiYK4rwFrDhqVdBZa3WBuZTwPo9DkuEiMYnac1B3dYlkGBqLUI2TAPF00NxhjZHAS', {
   apiVersion: "2023-08-16",
   typescript: false,
 });
